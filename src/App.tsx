548bae443bdf0757191b5b05ecc578cf7b4b7f93
JByteCraft <joaquinjhannchrist@gmail.com>
2025-11-10 03:21:56 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/LoginPage/Login";
+import Home from "./pages/HomePage/Home";
 
 export function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<div>Home Page</div>} />
+        <Route path="/*" element={<Home />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
     </Router>
---
+++ src/pages/HomePage/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-blue-200 via-white to-blue-200">
+      <h1 className="text-6xl font-bold text-gray-900">This is home page</h1>
+    </div>
+  );
+};
+
+export default Home;
